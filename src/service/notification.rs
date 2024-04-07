c78ba2ba31bf9316f82306b9185a0facfadbd661
Ammar Zufar <amzufar@gmail.com>
2024-04-07 16:58:49 +0700

    Implement receive_notification function in Notification service

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -89,4 +89,9 @@ impl NotificationService {
             ))
         }
     }
+
+    pub fn receive(payload: Notification) -> Result<Notification> {
+        let subscriber_result: Notification = NotificationRepository::add(payload);
+        return Ok(subscriber_result);
+    }
 }
